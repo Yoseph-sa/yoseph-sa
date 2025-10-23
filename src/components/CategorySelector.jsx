5cd545af239fca8c32a6ee274e4c2c0971d76642
Raghav Chhabra <91244493+erraghavchhabra@users.noreply.github.com>
2025-10-23 17:30:46 +0530

    udpate code category

--- src/components/CategorySelector.jsx
+++ src/components/CategorySelector.jsx
@@ -8,9 +8,10 @@ const CategorySelector = ({ categories = [], activeTab, setActiveTab }) => {
     <div className="category-selector d-flex flex-wrap gap-2">
       {/* All Tab */}
       <button
-        className={`category-btn  ${activeTab === "All" ? "active" : ""}`}
+        className={`category-btn  ${activeTab === "All" ? "active" : ""} ${
+          theme === "dark" ? "category-btn-dark" : "category-btn-light"
+        }`}
         onClick={() => setActiveTab("All")}
-        style={{ color: theme === "dark" ? "" : "#555" }}
       >
         All
       </button>
@@ -19,9 +20,10 @@ const CategorySelector = ({ categories = [], activeTab, setActiveTab }) => {
       {categories.map((cat, index) => (
         <button
           key={index}
-          className={`category-btn ${activeTab === cat.name ? "active" : ""}`}
+          className={`category-btn ${activeTab === cat.name ? "active" : ""} ${
+            theme === "dark" ? "category-btn-dark" : "category-btn-light"
+          }`}
           onClick={() => setActiveTab(cat.name)}
-          style={{ color: theme === "dark" ? "" : "#555" }}
         >
           {cat.name}
         </button>
